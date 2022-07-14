4e4c8d637f6c1ec8dba90ca9e38eba2a56a86182
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-07-14 17:06:36 +0500

    objects

--- play.js
+++ play.js
@@ -1,7 +1,8 @@
-let name='arh';
-let age=22;
-let hobbies=true;
-let funcName=(name,age)=>{
-    return name+age;
-}
-console.log(funcName(name,age));
+const person={
+    name:'max',
+    greet() {
+        console.log(this.name);
+    }
+};
+
+person.greet();
