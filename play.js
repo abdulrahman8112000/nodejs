66b37312c4afdbe8bb05edeaed27945fc0103c20
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-07-14 14:58:46 +0500

    refreshing the core syntax)

--- play.js
+++ play.js
@@ -2,3 +2,8 @@ var name='arh';
 var age=22;
 var hobbies=true;
 
+function userSummary(name ,age,hobby){
+    return ('name: '+name+'age: '+'hobbies: '+hobby);
+}
+
+console.log(userSummary(name,age,hobbies));
