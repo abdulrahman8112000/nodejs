334097f7c447aa4dbca6002d4d214ade59281f86
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-07-14 18:07:05 +0500

    arrays objects and reference types

--- play.js
+++ play.js
@@ -1,9 +1,4 @@
 const hobbies=['abc',1,true];
 
-for (let hobby of hobbies){
-    console.log(hobby);
-};
-
-console.log(hobbies.map((hobby)=>{
-    return "hobby: "+hobby;
-}));
+hobbies.push('xys');
+console.log(hobbies);
