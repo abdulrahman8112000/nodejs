b32ab49784b15afd449fe0e603e8ed9dbd297743
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-07-14 18:52:14 +0500

    async code and promises

--- play.js
+++ play.js
@@ -1,21 +1,3 @@
-const person={
-    name:'arh',
-    age:22,
-    greet() {
-        console.log(this.name);
-    }
-}
-
-const personName=({name})=>{
-    console.log(name);
-
-}
-
-const {name,age}=person;
-personName(person);
-console.log(name,age);
-
-const hobbies=[1,2,3,4,5];
-
-const [h1,h2]=hobbies;
-console.log(h1,h2);
+setTimeout(()=>{
+    console.log("hellor");
+},2000);
