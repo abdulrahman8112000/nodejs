810571e6fc6d56f478265b95523e0307698a80a0
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-07-15 16:08:27 +0500

    routing requests

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const http=require('http');
 
 
 const server=http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
+    const url=req.url;
+    if (url==='/'){
+        res.write('<html><head></head><body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>');
+        return res.end();
+    }
     res.setHeader('Content-Type','text/html');
     res.write('<html><head>My first page</head><body><h1>my first node js server</h1></body></html>');
     res.end();
