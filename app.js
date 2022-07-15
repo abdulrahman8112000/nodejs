5729304e21adeeec48fdc6560a7a1e5087902a17
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-07-15 15:38:59 +0500

    sending responses

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const http=require('http');
 
 const server=http.createServer((req,res)=>{
     console.log(req.url,req.method,req.headers);
-    process.exit();
+    res.setHeader('Content-Type','text/html');
+    res.write('<html><head>My first page</head><body><h1>my first node js server</h1></body></html>');
+    res.end();
+    
 });
 
 server.listen(3000);
