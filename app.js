ac9a397aa29a3da81230a75337b2d4ac3751815d
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-07-17 17:56:04 +0500

    sharing data accross requests and users

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const path=require('path');
-const adminRoutes=require('./routes/admin.js');
+const adminData=require('./routes/admin.js');
 const shopRoutes=require('./routes/shop.js');
 
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
-app.use('/admin',adminRoutes);
+app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const path=require('path');
+const adminData=require('./admin.js');
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
+    console.log(adminData.products);
     res.sendFile(path.join(__dirname,'../','views','shop.html'));
    
 });
