11e93ac05a822d213332b70b7bf441831e7f76e8
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-07-17 12:58:34 +0500

    using express router

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const express=require('express');
 const bodyParser=require('body-parser');
-const http=require('http');
+
+const adminRoutes=require('./routes/admin.js');
+const shopRoutes=require('./routes/shop.js');
 
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-    
-    res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-    
-})
-app.use('/',(req,res,next)=>{
-    res.send('<h1>express</h1>');
-   
-});
 app.listen(3000);
