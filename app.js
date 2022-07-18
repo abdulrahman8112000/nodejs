abb16f761c51186ecac94dc96d07e57b247ac78c
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-07-18 15:06:22 +0500

    converting our project to handlebars

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const bodyParser=require('body-parser');
 const path=require('path');
 const adminData=require('./routes/admin.js');
 const shopRoutes=require('./routes/shop.js');
-
+const expressHbs=require('express-handlebars');
 const app=express();
-
-app.set('view engine', 'pug');
+app.engine('hbs',expressHbs());
+app.set('view engine', 'hbs');
 app.set('views','views');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
@@ -14,7 +14,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname,'views','404.html'));
+    res.render('404',{pageTitle:'page not found'});
 })
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const router=express.Router();
 
 router.get('/',(req,res,next)=>{
     const products=adminData.products;
-    res.render('shop',{prods:products,docTitle:'shop'});
+    res.render('shop',{prods:products,pageTitle:'shop',path:'/',hasProducts:products.length>0});
    
    
 });
