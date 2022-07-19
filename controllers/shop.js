88618760a985bfcfa5e345c8799226a433ef4f1d
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-07-19 17:07:50 +0500

    adding another item

--- controllers/shop.js
+++ controllers/shop.js
@@ -21,6 +21,10 @@ exports.getCart=(req,res,next)=>{
   res.render('shop/cart',{path:'/cart',pageTitle:'Your cart'})
 }
 
+exports.getOrders=(req,res,next)=>{
+  res.render('shop/orders',{path:'/orders',pageTitle:'Your orders'})
+}
+
 exports.getCheckout=(req,res,next)=>{
   res.render('shop/checkout',{path:'/checkout',pageTitle:'Checkout'})
 }
