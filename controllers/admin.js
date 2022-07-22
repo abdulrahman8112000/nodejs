d23c2091620b8a7cd008ee0b90f4e58cad6c1778
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-07-22 17:04:13 +0500

    fetching related products

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
-    description: description
+    description: description,
+    
   })
     .then(result => {
       // console.log(result);
@@ -71,7 +72,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then(products => {
+  req.user.getProducts().then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
